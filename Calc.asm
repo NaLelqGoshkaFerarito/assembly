50f357259ddd0000afa5643c919c6437add5bb49
Stephen Wright <wrightsteven93@gmail.com>
2022-12-14 11:05:12 +0100

    Revert "minor fixes to menu and division added"
    This reverts commit 102a302b2185573a01432a1fc2a58a4639effd10.

--- Calc.asm
+++ Calc.asm
@@ -1,103 +1,7 @@
 ; Filename: calculator.asm
-; this code was made by Vladislav Serafimov and Stephen Cruz Wright
-
-;macros here	
-;output to stdout
-;args: input variable, length
-%macro print 2
-	mov rax, 1         ;id
-	mov rdi, 1         ;stdout used
-	mov rsi, %1        ;data
-	mov rdx, %2 	   ;length
-	syscall	           ;kernel call
-%endmacro
-
-;read from stdin
-;args: output variable
-%macro read 1
-	mov rax, 0         ;id
-	mov rdi, 0         ;stdin used
-	mov rsi, %1        ;variable
-	mov rdx, 2   	   ;length of variable
-	syscall
-%endmacro
-
-;addition macro
-;args: number 1, number 2
-%macro addition 2
-	; Perform the addition
-    mov rax, %1
-    mov rbx, %2
-	
-	sub rax, 48
-	sub rbx, 48
-	
-	add rax, rbx
-	
-	add rax, 48
-	
-	mov [answer], rax
-	syscall
-%endmacro
-
-;subtraction macro
-;args: number 1, number 2
-%macro subtraction 2
-	mov rax, %1
-	sub rax, 48
-	mov rbx, %2
-	sub rbx, 48
-	sub rax, rbx
-	add rax, 48
-	mov [answer], rax
-	syscall
-%endmacro
-
-;multiplication macro
-;args: number 1, number 2
-%macro multiplication 2
-	;mov rax, %1
-	;mul rax, %2
-	;mov [answer], rax
-%endmacro
-
-;division macro
-;args: number 1, number 2
-%macro division 2
-;	mov rax, %1 ; move the first operand into rax register
-;	mov rbx, %2 ; move the second operand into rbx register
-;	cqo ; sign-extend rax into rdx:rax
-;	idiv rbx ; divide rdx:rax by rbx
-	
-;	cmp rdx, 0 ; check if the remainder is zero
-;	jz roundDown ; if the remainder is zero, round down
-	
-;	cmp rdx, rbx/2 ; check if the remainder is greater than half of the divisor
-;	jg roundUp ; if the remainder is greater than half of the divisor, round up
-;	jmp result ; otherwise, return the result as is
-	
-;	roundUp:
-;	inc rax ; increment the result by 1 to round up
-;	jmp result ; return the result
-	
-;	roundDown:
-	;no need to round down
-	
-;	result:
-;	mov %0, rax ; move the result into the destination register
-	
-;	syscall
-%endmacro
-
-;just exit the program
-%macro quit 0
-	mov rax, 60        ;id
-	mov rdi, 0         ;exit code
-	syscall
-%endmacro
-
+;this code was made by Vladislav Serafimov and Stephen Cruz Wright
 section .data
-	intro db "------------Calculator------------", 10
+	intro db "------------Calculator------------"
 	introLen equ $-intro
 	sNum1 db 10, "Please enter your first number: "
 	sNum1Len equ $-sNum1
@@ -108,12 +12,67 @@ section .data
 	ans db 10, "Answer:"
 	ansLen equ $-ans
 	
-section .bss ;used to reserve data
-	var1 resb 2 ;reserve 2 bytes for var 1
+section .bss
+	var1 resb 2
 	var2 resb 2
 	choice resb 2
 	answer resb 2
 	
+;output to stdout
+;args: variable
+;args: variable, length
+%macro print 2
+	mov rax, 1         ;id
+	mov rdi, 1         ;stdout used
+	mov rsi, %1        ;data
+	mov rdx, %2 	   ;length
+	syscall	           ;kernel call
+%endmacro
+
+;read from stdin
+;args: variable, length
+%macro read 1
+	mov rax, 0         ;id
+	mov rdi, 0         ;stdin used
+	mov rsi, %1        ;variable
+	mov rdx, 8   	   ;length of variable
+	syscall    
+%endmacro
+
+;addition macro
+%macro addition 2
+	; Perform the addition
+    mov eax, %1
+    add eax, %2
+	syscall
+%endmacro
+
+;subtraction macro
+%macro subtraction 2
+	mov eax, %1
+	sub eax, %2
+	syscall
+%endmacro
+
+;multiplication macro
+%macro multiplication 2
+	mov eax, %1
+	imul eax, %2
+	syscall
+%endmacro
+
+;division macro
+%macro division 2
+	; Code here
+%endmacro
+
+;just exit the program
+%macro quit 0
+	mov rax, 60        ;id
+	mov rdi, 0         ;exit code
+	syscall
+%endmacro
+
 section .text
 	global _start
 _start:
@@ -130,8 +89,8 @@ _start:
 		
 		print oops, oopsLen		;displays choices
 		read choice  		;reads user's choice
-		mov ah, [choice]        ; Move the selected option []as a pointer] to the registry ah
-		sub ah, 48     		; Convert from ascii to decimal
+		mov ah, [choice]        ; Move the selected option to the registry ah
+		sub ah, '0'     		; Convert from ascii to decimal
 		
 		cmp ah, 1
 		je Addition
@@ -164,11 +123,15 @@ _start:
 		division var1, var2
 		jmp Print ;jumps to printer
 	
+		
 	Print:
 		
 		print ans, ansLen
+
+		mov rax, [answer]
+		sub rax, '0'
 		
-		print answer, 8 
+		print answer, 2
 		jmp StartofLoop	;restarts loop
 		
 	Quit:
